ae4e42ae3497873505ff16f9ee93da2697970ad8
Jaydin_MacBook <JaydinFreemanWork@gmail.com>
2023-06-27 15:39:43 -0400

    Added more complex functions to account for guessed letters

--- brain.py
+++ brain.py
@@ -23,6 +23,20 @@ def insert_word(word):
     conn.commit()
     conn.close()
 
+def update_guessed_letters(word, guessed_letters):
+    conn = sqlite3.connect('store.db')
+    c = conn.cursor()
+    c.execute("UPDATE hangman SET guessed_letters = ? WHERE word = ?", (guessed_letters, word))
+    conn.commit()
+    conn.close()
+
+def get_guessed_letters(word):
+    conn = sqlite3.connect('store.db')
+    c = conn.cursor()
+    c.execute("SELECT guessed_letters FROM hangman WHERE word = ?", (word,))
+    guessed_letters = c.fetchone()[0]
+    conn.close()
+    return guessed_letters
 
 if __name__ == "__main__":
     create_database()
