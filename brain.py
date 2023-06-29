0d18acb51b4c4cd99efd5bfc5a3d78afc5520630
Axel Cazorla <axel.cazorla10@gmail.com>
2023-06-29 12:29:01 -0400

    Updated Feature To interact with Inputs

--- brain.py
+++ brain.py
@@ -44,8 +44,21 @@ def get_guessed_letters(word):
     conn.close()
     return guessed_letters
 
+# Added inputs for the players      Axel Cazorla 06/29/2023
+
+def get_players(word):
+    conn = sqlite3.connect('store.db')
+    c = conn.cursor()
+    c.execute("SELECT player1, player2 FROM hangman WHERE word = ?", (word,))
+    players = c.fetchone()
+    conn.close()
+    return players
+
+# Updated Main to Interact with the New Inputs      Axel Cazorla 06/29/2023
 
 if __name__ == "__main__":
     create_database()
+    player1 = input("Enter Player 1 name: ")
+    player2 = input("Enter Player 2 name: ")
     word = get_random_word()
-    insert_word(word)
+    insert_word(word, player1, player2)
