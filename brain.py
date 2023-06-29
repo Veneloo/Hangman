e7632a9fe97783cff123752dd63659f6688fa4c8
Axel Cazorla <axel.cazorla10@gmail.com>
2023-06-29 13:24:18 -0400

    Added Leaderboard Features

--- brain.py
+++ brain.py
@@ -44,21 +44,59 @@ def get_guessed_letters(word):
     conn.close()
     return guessed_letters
 
-# Added inputs for the players      Axel Cazorla 06/29/2023
+# Added Leaderboard Features                Axel Cazorla 06/29/2023
 
-def get_players(word):
+def update_score(word, player_name, score):
     conn = sqlite3.connect('store.db')
     c = conn.cursor()
-    c.execute("SELECT player1, player2 FROM hangman WHERE word = ?", (word,))
-    players = c.fetchone()
+    c.execute("UPDATE hangman SET player_name = ?, score = ? WHERE word = ?",
+              (player_name, score, word))
+    conn.commit()
     conn.close()
-    return players
+
+
+def add_to_leaderboard(player_name, score):
+    conn = sqlite3.connect('store.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO leaderboard VALUES (?, ?)", (player_name, score))
+    conn.commit()
+    conn.close()
+
+
+def get_leaderboard():
+    conn = sqlite3.connect('store.db')
+    c = conn.cursor()
+    c.execute("SELECT player_name, score FROM leaderboard ORDER BY score ASC")
+    leaderboard = c.fetchall()
+    conn.close()
+    return leaderboard
+
 
 # Updated Main to Interact with the New Inputs      Axel Cazorla 06/29/2023
 
 if __name__ == "__main__":
     create_database()
-    player1 = input("Enter Player 1 name: ")
-    player2 = input("Enter Player 2 name: ")
     word = get_random_word()
-    insert_word(word, player1, player2)
+    insert_word(word)
+
+    player_name = input("Enter your name: ")
+    start_time = time.time()  # Start timer
+
+    # Game logic to resolve the word goes here
+    resolved_word = "python"  # Placeholder for resolved word
+
+    end_time = time.time()  # Stop timer
+    elapsed_time = end_time - start_time
+    score = elapsed_time  # Lower elapsed time means a better score
+
+    # Update the score for the player
+    update_score(word, player_name, score)
+
+    # Add player to leaderboard
+    add_to_leaderboard(player_name, score)
+
+    # Retrieve updated leaderboard
+    leaderboard = get_leaderboard()
+    print("Leaderboard:")
+    for position, (player, score) in enumerate(leaderboard, start=1):
+        print(f"{position}. {player}: {score:.2f} seconds")
